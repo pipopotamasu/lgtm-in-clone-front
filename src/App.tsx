91cea469a747adc8f17ecd834e3bafc6c8e4e42b
pipopotamasu <ec0156hx@gmail.com>
2020-01-22 08:20:08 +0900

    base eslint setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        header
-      </header>
+      <header className="App-header">header</header>
       <div>content</div>
     </div>
   );
-}
+};
 
 export default App;
