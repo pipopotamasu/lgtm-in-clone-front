30f812a1c62216784e98d0e189eb189564bbc58e
pipopotamasu <ec0156hx@gmail.com>
2020-01-25 22:37:22 +0900

    base header design

--- src/components/organisms/GlobalHeader.tsx
+++ src/components/organisms/GlobalHeader.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import { color, width } from '../../constants/cssVariables';
+
+const Header = styled.header`
+  width: 100%;
+  background-color: ${color.bg.gray};
+  border-bottom: 1px solid ${color.border.gray};
+`
+
+const Nav = styled.nav`
+  width: ${width.base};
+  margin: 0 auto;
+`
+
+const List = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+const Item = styled.li<{right?: boolean}>`
+  color: ${color.text.gray};
+  padding: 0.8rem 1rem;
+  cursor: pointer;
+  ${({ right }) => right ? 'margin-left: auto;' : ''}
+
+  &:hover {
+    color: ${color.text.black};
+  }
+`
 
 const GlobalHeader: React.FC = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Randome</li>
-          <li>Submit</li>
-          <li>Browse</li>
-          <li>Profile</li>
-        </ul>
-      </nav>
-    </header>
+    <Header>
+      <Nav>
+        <List>
+          <Item>Home</Item>
+          <Item>Random</Item>
+          <Item>Submit</Item>
+          <Item>Browse</Item>
+          <Item right>Login</Item>
+        </List>
+      </Nav>
+    </Header>
   );
 };
 
---
+++ src/constants/cssVariables.ts
@@ -0,0 +1,16 @@
+export const width = {
+  base: '1080px'
+}
+
+export const color = {
+  bg: {
+    gray: '#f8f8f8',
+  },
+  border: {
+    gray: '#e7e7e7',
+  },
+  text: {
+    gray: '#777',
+    black: 'black',
+  }
+}
