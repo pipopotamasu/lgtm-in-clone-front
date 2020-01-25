dfa5e550ab910d3849bb8a0d2fb2054d1208e8fc
pipopotamasu <ec0156hx@gmail.com>
2020-01-25 22:42:24 +0900

    Global header component (#1)
    * sort out initial files
    
    * add reset css
    
    * base eslint setting
    
    * eslint react setting
    
    * eslint a11y
    
    * base global header component
    
    * base header design

---
+++ src/components/organisms/GlobalHeader.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { color, width } from '../../constants/cssVariables';
+
+const Header = styled.header`
+  width: 100%;
+  background-color: ${color.bg.gray};
+  border-bottom: 1px solid ${color.border.gray};
+`
+
+const Nav = styled.nav`
+  width: ${width.base};
+  margin: 0 auto;
+`
+
+const List = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+const Item = styled.li<{right?: boolean}>`
+  color: ${color.text.gray};
+  padding: 0.8rem 1rem;
+  cursor: pointer;
+  ${({ right }) => right ? 'margin-left: auto;' : ''}
+
+  &:hover {
+    color: ${color.text.black};
+  }
+`
+
+const GlobalHeader: React.FC = () => {
+  return (
+    <Header>
+      <Nav>
+        <List>
+          <Item>Home</Item>
+          <Item>Random</Item>
+          <Item>Submit</Item>
+          <Item>Browse</Item>
+          <Item right>Login</Item>
+        </List>
+      </Nav>
+    </Header>
+  );
+};
+
+export default GlobalHeader;
