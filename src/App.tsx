df4f2613d30240c8be8ab1ec610b7da7f6904467
pipopotamasu <ec0156hx@gmail.com>
2020-02-07 23:47:34 +0900

    browse (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import GlobalMessages from './components/organisms/GlobalMessages';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Random from './pages/Random';
+import Browse from './pages/Browse';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './reducers/store';
@@ -32,7 +33,9 @@ const App: React.FC = () => {
                 <Random />
               </Route>
               <Route path="/submit">Submit</Route>
-              <Route path="/browse">Browse</Route>
+              <Route path="/browse">
+                <Browse />
+              </Route>
               <Route path="/posts/:id">
                 <Detail />
               </Route>
---
+++ src/pages/Browse.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LgtmCards from '../components/organisms/LgtmCards';
+
+const Browse: React.FC = () => {
+  return (
+    <div>
+      <LgtmCards />
+    </div>
+  );
+};
+
+export default Browse;
