a45cd10c0d69dcda7463ef2a5b176e681b876f10
pipopotamasu <ec0156hx@gmail.com>
2020-04-30 02:24:19 +0900

    lint

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env!.REACT_APP_MOCK_SERVER === '1' ? 'http://localhost:4000' : 'http://localhost:8000'
+const baseURL =
+  process.env!.REACT_APP_MOCK_SERVER === '1'
+    ? 'http://localhost:4000'
+    : 'http://localhost:8000';
 
 export default axios.create({
   baseURL,
