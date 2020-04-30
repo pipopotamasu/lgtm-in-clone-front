2ece54c5f5f58965b0abe78b7dc272950aea7d78
pipopotamasu <ec0156hx@gmail.com>
2020-05-01 00:04:07 +0900

    Switch host (#43)
    * switch backend host
    
    * lint

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env!.REACT_APP_MOCK_SERVER === '1'
+    ? 'http://localhost:4000'
+    : 'http://localhost:8000';
+
 export default axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL,
 });
