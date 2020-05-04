e405a08f651b0795342b6f4124ec80b1e3bf610a
pipopotamasu <ec0156hx@gmail.com>
2020-05-04 16:43:19 +0900

    refactor hooks

--- src/hooks/useFetchPost.ts
+++ src/hooks/useFetchPost.ts
@@ -1,26 +1,26 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useContext } from 'react';
 import { useDispatch } from 'react-redux';
-import { handleErrorMessage as handleErrorMessageCreator } from 'src/actions/globalMessages';
-import { fetchPost as fetchPostCreator } from 'src/actions/posts';
-import postsService from 'src/services/post';
+import { fetchPost as fetchPostActionCreator } from 'src/actions/posts';
+import { RootContext } from 'src/contexts/root';
 
 export default function useFetchPost() {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
+  const { $api, $notification } = useContext(RootContext);
 
   const fetchPost = useCallback(
     async (id: number) => {
       setLoading(true);
       try {
-        const res = await postsService.getPost(id);
+        const res = await $api.post.getPost(id);
         setLoading(false);
-        dispatch(fetchPostCreator(res.data));
+        dispatch(fetchPostActionCreator(res.data));
       } catch (e) {
         setLoading(false);
-        dispatch(handleErrorMessageCreator(e.message));
+        $notification.error(e.message);
       }
     },
-    [dispatch]
+    [dispatch, $api, $notification]
   );
   return { fetchPost, loading };
 }
--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -16,7 +16,29 @@ export function error(message: string) {
   });
 }
 
+export function info(message: string) {
+  toast(message, {
+    position: 'top-center',
+    autoClose: 5000,
+    type: 'info',
+  });
+}
+
+export function warning(message: string) {
+  toast(message, {
+    position: 'top-center',
+    autoClose: 10000,
+    type: 'warning',
+  });
+}
+
 export const $notification = {
   success,
   error,
-} as Readonly<{ success: typeof success; error: typeof error }>;
+  info,
+} as Readonly<{
+  success: typeof success;
+  error: typeof error;
+  info: typeof info;
+  warning: typeof warning;
+}>;
